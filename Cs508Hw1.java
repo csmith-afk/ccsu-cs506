c547d5883dd49358f72e885e486afbd685915df5
csmith-afk <csmith645@live.com>
2020-02-06 05:29:13 +0000

    update

--- Cs508Hw1.java
+++ Cs508Hw1.java
@@ -3,28 +3,28 @@ public class Cs508Hw1 {
         Runnable oneToTen =() -> {
             int sum=0;
             for (int i = 1; i<=10; i++) {
-                System.out.println(i + "Thread ID:" + Thread.currentThread().getId());
+                System.out.println(i + " Thread ID: " + Thread.currentThread().getId());
                 sum=sum+i;
             }
-            System.out.println("Sum: " + sum + "Thread ID: " + Thread.currentThread().getId());
+            System.out.println("Sum: " + sum + " Thread ID: " + Thread.currentThread().getId());
         };
 
         Runnable mediumInt =() -> {
             int sum=0;
             for (int i = (int) Math.pow(11,2); i<=Math.pow(20,2); i++) {
-                System.out.println(i + "Thread ID: " + Thread.currentThread().getId());
+                System.out.println(i + " Thread ID: " + Thread.currentThread().getId());
                 sum=sum+i;
             }  
-            System.out.println("Sum: " + sum + "Thread ID: " + Thread.currentThread().getId());
+            System.out.println("Sum: " + sum + " Thread ID: " + Thread.currentThread().getId());
         };
 
         Runnable largeInt =() -> {
             int sum=0;
             for(int i = (int) Math.pow(21,3); i<=Math.pow(30,3); i++) {
-                System.out.println(i + "Thread ID: " + Thread.currentThread().getId());
+                System.out.println(i + " Thread ID: " + Thread.currentThread().getId());
                 sum=sum+i;
             }
-            System.out.println("Sum: " + sum + "Thread ID: " + Thread.currentThread().getId());
+            System.out.println("Sum: " + sum + " Thread ID: " + Thread.currentThread().getId());
         };
 
         Thread one = new Thread(oneToTen);
